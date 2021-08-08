d80926c14b941005a675700bfb788bf8c0554bb5
Ebod <Macbook@Ebaads-MBP.broadband>
2021-08-08 02:50:47 +0100

    make it better

--- frontpageHot.js
+++ frontpageHot.js
@@ -6,7 +6,7 @@ const hook = new Webhook("https://discord.com/api/webhooks/871789208610693211/HW
 let previousValue;
  
 main();
-setInterval(main, 2000);
+setInterval(main, 1000);
 
 async function fetchHTML(url) {
   const { data } = await axios.get(url)
@@ -14,7 +14,22 @@ async function fetchHTML(url) {
 }
 
 async function main () {
-  uh()
+  const $ = await fetchHTML("https://slickdeals.net/newsearch.php?pp=20&sort=newest&previousdays=1&forumid%5b%5d=71&forumid%5b%5d=25&forumid%5b%5d=9&forumid%5b%5d=30&forumid%5b%5d=53&forumid%5b%5d=4&forumid%5b%5d=54&forumid%5b%5d=10&forumid%5b%5d=38&forumid%5b%5d=39&forumid%5b%5d=8&forumid%5b%5d=13&r=1")
+  let newValue = $('div.resultRow:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1)').text()
+  let newItemLink =  $('div.resultRow:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1)').attr('href')
+  let oldValue = $('div.resultRow:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1)').text()
+  let oldItemLink = $('div.resultRow:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1)').attr('href')
+
+  if( typeof previousValue === 'undefined' ){
+    console.log(`Latest item: ${newValue}, link is https://slickdeals.net${newItemLink}`);
+  } else if( previousValue !== newValue){
+    console.log(`New item:  ${newValue}, link is https://slickdeals.net${newItemLink}`);
+    console.log(Date());
+    oldWebhook(oldItemLink, oldValue)
+    newWebhook(newItemLink, newValue)
+  }
+
+  previousValue = newValue;
 }
 
 async function newWebhook(url, item) {
@@ -46,16 +61,4 @@ async function oldWebhook(url, item) {
   hook.send(embed);
 }
 
-async function uh() {
-  const embed = new MessageBuilder()
-  .setTitle('Old content')
-  .setAuthor('BB Monitors', '')
-  .addField(`~~Old post url:~~`, `~~https://www.slickdeals.net~~`, true)
-  .setColor('#FF0000')
-  .setThumbnail('https://cdn.discordapp.com/icons/718372968383643678/de8ebf56f71dccba06039dcd27426e30.webp?size=256')
-  .setDescription(`~~~~`)
-  .setFooter('', 'https://cdn.discordapp.com/icons/718372968383643678/de8ebf56f71dccba06039dcd27426e30.webp?size=256')
-  .setTimestamp();
 
-  hook.send(embed);
-}
